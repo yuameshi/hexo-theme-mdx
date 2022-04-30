99b546a18405634a654ccf6990c206210663f3af
Yuameshi <64469437+Yuameshi@users.noreply.github.com>
2022-04-30 18:06:17 +0800

    0.1.7 & 浏览器支持到IE10，删除了部分非必须的内联CSS，修改部分拼写错误

--- source/js/material.js
+++ source/js/material.js
@@ -1,42 +1,21 @@
 document.addEventListener('DOMContentLoaded', function () {
-	document
-		.querySelectorAll('div#mainContent > div.pageInition > *')
-		.forEach(function (el) {
-			if (el.classList.contains('current') == false) {
-				if (el.classList.contains('space') == false) {
-					el.classList.add('mdui-ripple');
-				}
-			} else {
-				el.classList.add('mdui-color-theme');
-			}
-		});
-	document
-		.querySelector('button#goToTopBtn')
-		.addEventListener('click', function () {
-			window.scroll({ top: 0, left: 0, behavior: 'smooth' });
-		});
+	paginationInit();
+	initBackToTopBtn();
 });
 
 window.addEventListener('load', function () {
-	document
-		.querySelectorAll('div#mainContent > div.pageInition > *')
-		.forEach(function (el) {
-			if (el.classList.contains('current') == false) {
-				if (el.classList.contains('space') == false) {
-					el.classList.add('mdui-ripple');
-				}
-			} else {
-				el.classList.add('mdui-color-theme');
-			}
-		});
-	document
-		.querySelector('button#goToTopBtn')
-		.addEventListener('click', function () {
-			window.scroll({ top: 0, left: 0, behavior: 'smooth' });
-		});
-	document.querySelector('#pageLoadProgressIndicator').remove();
+	paginationInit();
+	initBackToTopBtn();
+	var pageLoadProgressIndicator = document.querySelector(
+		'#pageLoadProgressIndicator'
+	);
+	if (pageLoadProgressIndicator.parentElement) {
+		pageLoadProgressIndicator.parentElement.removeChild(
+			pageLoadProgressIndicator
+		);
+	}
 });
-	
+
 document.addEventListener('scroll', function () {
 	if ((window.scrollY || window.pageYOffset) > window.screenY / 2) {
 		document
@@ -48,3 +27,36 @@ document.addEventListener('scroll', function () {
 			.classList.add('mdui-fab-hide');
 	}
 });
+
+function initBackToTopBtn() {
+	if (!window.ifInitiatedGoToTopBtn) {
+		document
+			.querySelector('button#goToTopBtn')
+			.addEventListener('click', function () {
+				window.scroll({ top: 0, left: 0, behavior: 'smooth' });
+			});
+		window.ifInitiatedGoToTopBtn = true;
+	}
+}
+
+function paginationInit() {
+	if (document.querySelector('div.pagination')) {
+		var paginations = document.querySelectorAll(
+			'div#mainContent > div.pagination > *'
+		);
+		for (var i = 0; i < paginations.length; i++) {
+			if (paginations[i].classList.contains('current') == false) {
+				if (paginations[i].classList.contains('space') == false) {
+					paginations[i].classList.add('mdui-ripple');
+				}
+			} else {
+				paginations[i].classList.add('mdui-color-theme');
+			}
+		}
+		document
+			.querySelector(
+				'div#mainContent > div.pagination > span.current.page-number'
+			)
+			.classList.add('mdui-ripple', 'mdui-color-theme');
+	}
+}
