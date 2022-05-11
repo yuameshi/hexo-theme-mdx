528010c4551d5559de6cd83d4f27a7b0549080a4
Yuameshi <64469437+Yuameshi@users.noreply.github.com>
2022-05-11 09:41:14 +0800

    0.2.8α：新增：在其他设备上继续阅读

--- source/js/post.js
+++ source/js/post.js
@@ -9,6 +9,34 @@ window.addEventListener('load', function () {
 	}
 });
 function initPost() {
+	document
+		.querySelector('#readOnOtherDeviceBtn')
+		.addEventListener('click', () => {
+			var dom = document.querySelector('#mdx_read_on_other_device');
+			if (typeof QRCode !== 'function') {
+				dom.innerHTML = 'Error: QRCode.js is not loaded.';
+				return;
+			}
+			dom.innerHTML = '';
+			var url = new URL(location.href);
+			new QRCode(dom, {
+				text: url.origin + url.pathname + '?pos=' + window.scrollY,
+				width: 128,
+				height: 128,
+				colorDark: '#000000',
+				colorLight: '#ffffff',
+				useSVG: true,
+				correctLevel: QRCode.CorrectLevel.H,
+			});
+			dom.querySelector('img').style.padding = '20px';
+		});
+	if (new URL(location.href).searchParams.get('pos') !== null) {
+		window.scroll({
+			top: Number(new URL(location.href).searchParams.get('pos')),
+			left: 0,
+			behavior: 'smooth',
+		});
+	}
 	// init table
 	const postPage = document.querySelector('#mainContent > div.postPage');
 	var tables = postPage.querySelectorAll('table');
