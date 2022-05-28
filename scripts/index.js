d3fbab4195554c930c0c6686a3e39af3e378ad9d
Yuameshi <64469437+Yuameshi@users.noreply.github.com>
2022-05-28 13:42:12 +0800

    0.4.1α：优化：使用CDN时自动删除mdui文件夹

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,17 @@
+const fs = require('fs');
+const path = require('path');
 console.log('Checking configurations...');
 
 if (hexo.config.search.path !== 'search.json') {
-	console.log(
-		'Search Database is not configured properly. Please edit it to `search.json`.'
-	);
+	console.log('Search Database is not configured properly. Please edit it to `search.json`.');
 }
+hexo.extend.filter.register(
+	'before_exit',
+	function () {
+		if (hexo.config.theme.cdn_provider !== false && hexo.config.theme.cdn_provider !== 'false') {
+			console.log('CDN enabled, deleting mdui folder.');
+			fs.rmSync(path.join(hexo.config.public_dir, 'mdui'), { recursive: true, force: true });
+		}
+	},
+	0
+);
