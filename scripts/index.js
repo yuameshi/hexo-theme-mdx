8bab03f04a04d6abc5656b768c2b11110df43500
Yuameshi <64469437+Yuameshi@users.noreply.github.com>
2022-07-29 23:59:09 +0800

    refactor(cdn):move cdn link definition to index.js

--- scripts/index.js
+++ scripts/index.js
@@ -5,6 +5,49 @@ console.log('[MDx]	[INFO]	Checking configurations...');
 if (hexo.config.theme_config.search.path && hexo.config.search.path !== hexo.config.theme_config.search.path) {
 	console.log('[MDx]	[ERROR]	Search Database is not configured properly. Please chech your settings.');
 }
+
+hexo.locals.set('linkPrefix', ()=>{
+	let linkPrefix = 'https://cdn.jsdelivr.net/npm/%package%@%version%/dist/';
+	switch (hexo.config.theme_config.cdn_provider) {
+		case 'cdnjs':
+			linkPrefix = 'https://cdnjs.cloudflare.com/ajax/libs/%package%/%version%/';
+			break;
+		case 'jsdelivr':
+			linkPrefix = 'https://cdn.jsdelivr.net/npm/%package%@%version%/dist/';
+			break;
+		case 'staticfile':
+			linkPrefix = 'https://cdn.staticfile.org/%package%/%version%/';
+			break;
+		case 'bootcdn':
+			linkPrefix = 'https://cdn.bootcdn.net/ajax/libs/%package%/%version%/';
+			break;
+		case 'bytedance':
+			linkPrefix = 'https://lf9-cdn-tos.bytecdntp.com/cdn/expire-1-y/%package%/%version%/';
+			break;
+		case 'unpkg':
+			linkPrefix = 'https://unpkg.com/%package%@%version%/dist/';
+			break;
+		case 'baomitu':
+			linkPrefix = 'https://lib.baomitu.com/%package%/%version%/';
+			break;
+		case 'true':
+			linkPrefix = 'https://cdn.jsdelivr.net/npm/%package%@%version%/dist/';
+			break;
+		case true:
+			linkPrefix = 'https://cdn.jsdelivr.net/npm/%package%@%version%/dist/';
+			break;
+		case 'false':
+			linkPrefix = '/%package%/';
+			break;
+		case false:
+			linkPrefix = '/%package%/';
+			break;
+		default:
+			break;
+	}
+	return linkPrefix;
+});
+
 hexo.extend.filter.register(
 	'before_exit',
 	function () {
@@ -20,3 +63,4 @@ hexo.extend.filter.register(
 	},
 	1
 );
+
